c0ef2cf55218f3551a68cb1480ebc0bf9eda83f9
qyt-l <3353273495@qq.com>
2026-03-08 17:45:44 +0800

    基于keras框架搭建的全连接神经网络模型训练并保存,绘制loss和acc图片

--- Deep_learning/Lab1/乳腺癌的分类检测/train.py
+++ Deep_learning/Lab1/乳腺癌的分类检测/train.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt #画图包
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 #导入leras框架
-import keras
-from keras.layers import Dense
-from keras.utils.np_utils import to_categorical
+from tensorflow import keras
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
 from sklearn.metrics import classification_report
 
 # 解决中文显示问题
@@ -21,10 +21,10 @@ X = dataset.iloc[:,:-1]
 Y = dataset.loc[ : ,'target']
 # print(Y)
 # 划分
-x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.2,random_state=42)
 # 转换为one-hot向量格式
-y_train = to_categorical(y_train, 2)
-y_test = to_categorical(y_test, 2)
+y_train_one = to_categorical(y_train, 2)
+y_test_one = to_categorical(y_test, 2)
 
 #数据归一化
 sc = MinMaxScaler(feature_range=(0,1))
@@ -32,5 +32,31 @@ x_train = sc.fit_transform(x_train)
 x_test = sc.transform(x_test)
 
 
+# 用Keras框架搭建深度学习网络模型
+model = keras.Sequential()
+model.add(Dense(10,activation='relu')) #Dense就是全连接网络模型
+model.add(Dense(10,activation='relu'))
+model.add(Dense(2,activation='softmax'))
 
+# 对神经网络进行编译
+model.compile(loss='categorical_crossentropy',optimizer='SGD',metrics=['accuracy'])
+#交叉熵 随机梯度下降法
 
+# 模型训练
+
+history = model.fit(x_train,y_train_one,epochs = 150,batch_size=16,verbose=2,validation_data=(x_test,y_test_one))
+model.save('model.h5')
+
+#绘制训练集和验证集的loss值对比
+plt.plot(history.history['loss'],label='train')
+plt.plot(history.history['val_loss'],label='val')
+plt.title("全连接神经网络loss值")
+plt.legend()
+plt.show()
+
+#绘制训练集和验证集准确率对比图
+plt.plot(history.history['accuracy'],label='train')
+plt.plot(history.history['val_accuracy'],label='val')
+plt.title("全连接神经网络loss值")
+plt.legend()
+plt.show()
