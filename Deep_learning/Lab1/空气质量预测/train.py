7c96f519d8adeb1a7714ce69aaad9dd19aef6e8f
qyt-l <3353273495@qq.com>
2026-03-09 20:30:51 +0800

    改报错sc.fit_transform需要二维数组,一维数组时使用加上.to_frame(),搭建模型并训练!

--- Deep_learning/Lab1/空气质量预测/train.py
+++ Deep_learning/Lab1/空气质量预测/train.py
@@ -29,7 +29,27 @@ x_train = sc_X.fit_transform(x_train)
 x_test = sc_X.transform(x_test)
 # print(y_test)
 sc_Y = MinMaxScaler(feature_range=(0,1))
-y_train = sc_Y.fit_transform(y_train)
-y_test = sc_Y.transform(y_test)
+# 修改报错
+y_train = sc_Y.fit_transform(y_train.to_frame())
+y_test = sc_Y.transform(y_test.to_frame())
 
+#···模型搭建
 
+model = keras.Sequential()
+model.add(Dense(10,activation='relu'))
+model.add(Dense(10,activation='relu'))
+# 回归任务输出一个值最后所得到的应该是一个值,不需要用激活函数
+model.add(Dense(1))
+# 模型编译
+# SGD优化器,loss函数选择mes
+model.compile(loss='mse',optimizer='SGD')
+history = model.fit(x_train,y_train,epochs = 100,batch_size=16,verbose=2,validation_data=(x_test,y_test))
+# verbose显示训练的过程
+model.save("model.h5")
+
+#绘制模型的训练集和验证集的loss值
+plt.plot(history.history['loss'],label='train')
+plt.plot(history.history['val_loss'],label='val')
+plt.title("全连接神经网络loss值")
+plt.legend()
+plt.show()
